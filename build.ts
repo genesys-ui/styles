6b70ae1eac8de720ae9697520cf169d1b7093b35
Javier López Úbeda <jlopezcur@gmail.com>
2024-04-15 11:12:06 +0200

    build: improved the build process
    - Added SCSS
    - Added cleaning process between builds

--- build.ts
+++ build.ts
@@ -1,6 +1,13 @@
 import sass from "sass";
-import { writeFileSync } from "fs";
+import { writeFileSync, cpSync, rmSync, mkdirSync } from "fs";
 
+// Prepare environment
+rmSync('dist', { recursive: true, force: true });
+mkdirSync('dist');
+
+// Create css
 const result = sass.compile("./src/styles.scss");
-
 writeFileSync("dist/styles.css", result.css);
+
+// Copy SCSS files
+cpSync("src/", "dist/", { recursive: true });
