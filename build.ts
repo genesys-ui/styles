9d500d9745c42d0998ec35bb8b8f3c51f7ec2bf2
jguede <116181206+jguede@users.noreply.github.com>
2024-06-25 17:42:38 +0200

    fix(QUV-2366): node modules imports paths have change to the root node modules (#5)
    * feat(QUV-2366): node modules imports paths have change to the root node_modules
    
    * build: add generate dist
    
    * fix: delete base styles
    
    ---------
    
    Co-authored-by: santiago.trigo <santiago.trigo@devo.com>

--- build.ts
+++ build.ts
@@ -7,11 +7,15 @@ mkdirSync('dist');
 mkdirSync('dist/css');
 
 // Create css
-const lightResult = sass.compile("./src/light.scss");
+const lightResult = sass.compile("./light.scss");
 writeFileSync("dist/css/light.css", lightResult.css);
 
-const darkResult = sass.compile("./src/dark.scss");
+const darkResult = sass.compile("./dark.scss");
 writeFileSync("dist/css/dark.css", darkResult.css);
 
 // Copy SCSS files
-cpSync("src/", "dist/scss", { recursive: true });
+cpSync("src/_styles.scss", "dist/scss/_styles.scss");
+cpSync("src/components", "dist/scss/components", { recursive: true });
+cpSync("src/layout", "dist/scss/layout", { recursive: true });
+cpSync("src/utils", "dist/scss/utils", { recursive: true });
+cpSync("src/vendors", "dist/scss/vendors", { recursive: true });
