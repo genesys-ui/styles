1f57519f69c9f7356a162419a2087039816430b5
Jose Andrés Granero <joseandres.granero@devo.com>
2025-07-14 13:30:57 +0200

    fix: Fixed divider mixin.

--- build.ts
+++ build.ts
@@ -13,8 +13,8 @@ writeFileSync("dist/css/light.css", lightResult.css);
 const darkResult = sass.compile("./dark.scss");
 writeFileSync("dist/css/dark.css", darkResult.css);
 
-//const globalResult = sass.compile("./global.scss");
-//writeFileSync("dist/css/styles.css", globalResult.css);
+const globalResult = sass.compile("./global.scss");
+writeFileSync("dist/css/styles.css", globalResult.css);
 
 // Copy SCSS files
 cpSync("src/_styles.scss", "dist/scss/_styles.scss");
